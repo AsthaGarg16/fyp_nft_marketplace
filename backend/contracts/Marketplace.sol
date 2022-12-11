fd33c0d79c971917dc088a73c429f4b47f99d684
AsthaGarg16 <asthagarg1611@gmail.com>
2022-12-11 15:15:51 +0530

    updates to the main contract

--- backend/contracts/Marketplace.sol
+++ backend/contracts/Marketplace.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 error PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
@@ -17,16 +18,14 @@ error PriceMustBeAboveZero();
 // error IsNotOwner()
 
 contract NftMarketplace is ReentrancyGuard {
-    struct Listing {
-        uint256 price;
-        address seller;
-    }
-
+    /// @notice Events for the contract
     event ItemListed(
         address indexed seller,
         address indexed nftAddress,
         uint256 indexed tokenId,
-        uint256 price
+        uint256 quantity,
+        uint256 price,
+        uint256 startingTime
     );
 
     event ItemCanceled(address indexed seller, address indexed nftAddress, uint256 indexed tokenId);
@@ -35,29 +34,109 @@ contract NftMarketplace is ReentrancyGuard {
         address indexed buyer,
         address indexed nftAddress,
         uint256 indexed tokenId,
-        uint256 price
+        uint256 price,
+        uint256 quantity
     );
 
-    mapping(address => mapping(uint256 => Listing)) private s_listings;
-    mapping(address => uint256) private s_proceeds;
+    event ItemUpdated(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 newPrice
+    );
 
-    modifier notListed(
-        address nftAddress,
-        uint256 tokenId,
-        address owner
+    event OfferCreated(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 quantity,
+        uint256 price,
+        uint256 deadline
+    );
+    event OfferCanceled(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId
+    );
+
+    event UpdatePlatformFee(uint16 platformFee);
+    event UpdatePlatformFeeRecipient(address payable platformFeeRecipient);
+
+    /// @notice Structure for listed items
+    struct Listing {
+        uint256 quantity;
+        uint256 price;
+        uint256 startingTime;
+    }
+
+    /// @notice Structure for offer
+    struct Offer {
+        uint256 quantity;
+        uint256 pricePerItem;
+        uint256 deadline;
+    }
+
+    struct CollectionRoyalty {
+        uint16 royalty;
+        address creator;
+        address feeRecipient;
+    }
+
+    /// @notice NftAddress -> Token ID -> Minter
+    mapping(address => mapping(uint256 => address)) public minters;
+
+    /// @notice NftAddress -> Token ID -> Royalty
+    mapping(address => mapping(uint256 => uint16)) public royalties;
+
+    /// @notice NftAddress -> Token ID -> Owner -> Listing item
+    mapping(address => mapping(uint256 => mapping(address => Listing))) public listings;
+
+    // mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    /// @notice NftAddress -> Token ID -> Offerer -> Offer
+    mapping(address => mapping(uint256 => mapping(address => Offer))) public offers;
+
+    /// @notice Platform fee
+    uint16 public platformFee;
+
+    /// @notice Platform fee receipient
+    address payable public feeReceipient;
+
+    /// @notice NftAddress -> Royalty
+    mapping(address => CollectionRoyalty) public collectionRoyalties;
+
+    // mapping(address => uint256) private s_proceeds;
+
+    modifier isListed(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _owner
     ) {
-        Listing memory listing = s_listings[nftAddress][tokenId];
-        if (listing.price > 0) {
-            revert AlreadyListed(nftAddress, tokenId);
-        }
+        Listing memory listing = listings[_nftAddress][_tokenId][_owner];
+        require(listing.quantity > 0, "not listed item");
         _;
     }
 
-    modifier isListed(address nftAddress, uint256 tokenId) {
-        Listing memory listing = s_listings[nftAddress][tokenId];
-        if (listing.price <= 0) {
-            revert NotListed(nftAddress, tokenId);
-        }
+    modifier notListed(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _owner
+    ) {
+        Listing memory listing = listings[_nftAddress][_tokenId][_owner];
+        require(listing.quantity == 0, "already listed");
+        _;
+    }
+
+    modifier validListing(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _owner
+    ) {
+        Listing memory listedItem = listings[_nftAddress][_tokenId][_owner];
+
+        _validOwner(_nftAddress, _tokenId, _owner, listedItem.quantity);
+
+        require(_getNow() >= listedItem.startingTime, "item not buyable");
         _;
     }
 
@@ -74,9 +153,34 @@ contract NftMarketplace is ReentrancyGuard {
         _;
     }
 
-    /////////////////////
+    modifier offerExists(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _creator
+    ) {
+        Offer memory offer = offers[_nftAddress][_tokenId][_creator];
+        require(offer.quantity > 0 && offer.deadline > _getNow(), "offer not exists or expired");
+        _;
+    }
+
+    modifier offerNotExists(
+        address _nftAddress,
+        uint256 _tokenId,
+        address _creator
+    ) {
+        Offer memory offer = offers[_nftAddress][_tokenId][_creator];
+        require(offer.quantity == 0 || offer.deadline <= _getNow(), "offer already created");
+        _;
+    }
+
+    constructor(address payable _feeRecipient, uint16 _platformFee) public {
+        platformFee = _platformFee;
+        feeReceipient = _feeRecipient;
+    }
+
+    ////////////////////
     // Main Functions //
-    /////////////////////
+    ////////////////////
     /*
      * @notice Method for listing NFT
      * @param nftAddress Address of NFT contract
@@ -84,10 +188,17 @@ contract NftMarketplace is ReentrancyGuard {
      * @param price sale price for each item
      */
     function listItem(
-        address nftAddress,
-        uint256 tokenId,
-        uint256 price
-    ) external notListed(nftAddress, tokenId, msg.sender) isOwner(nftAddress, tokenId, msg.sender) {
+        address _nftAddress,
+        uint256 _tokenId,
+        uint256 _quantity,
+        address _payToken,
+        uint256 _pricePerItem,
+        uint256 _startingTime
+    )
+        external
+        notListed(_nftAddress, _tokenId, msg.sender)
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
         if (price <= 0) {
             revert PriceMustBeAboveZero();
         }
@@ -104,11 +215,10 @@ contract NftMarketplace is ReentrancyGuard {
      * @param nftAddress Address of NFT contract
      * @param tokenId Token ID of NFT
      */
-    function cancelListing(address nftAddress, uint256 tokenId)
-        external
-        isOwner(nftAddress, tokenId, msg.sender)
-        isListed(nftAddress, tokenId)
-    {
+    function cancelListing(
+        address nftAddress,
+        uint256 tokenId
+    ) external isOwner(nftAddress, tokenId, msg.sender) isListed(nftAddress, tokenId) {
         delete (s_listings[nftAddress][tokenId]);
         emit ItemCanceled(msg.sender, nftAddress, tokenId);
     }
@@ -121,7 +231,10 @@ contract NftMarketplace is ReentrancyGuard {
      * @param nftAddress Address of NFT contract
      * @param tokenId Token ID of NFT
      */
-    function buyItem(address nftAddress, uint256 tokenId)
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    )
         external
         payable
         isListed(nftAddress, tokenId)
@@ -169,19 +282,26 @@ contract NftMarketplace is ReentrancyGuard {
         require(success, "Transfer failed");
     }
 
-    /////////////////////
+    //////////////////////
     // Getter Functions //
-    /////////////////////
+    //////////////////////
 
-    function getListing(address nftAddress, uint256 tokenId)
-        external
-        view
-        returns (Listing memory)
-    {
-        return s_listings[nftAddress][tokenId];
+    function getListing(
+        address nftAddress,
+        uint256 tokenId
+    ) external view returns (Listing memory) {
+        return Listing[nftAddress][tokenId];
     }
 
     function getProceeds(address seller) external view returns (uint256) {
         return s_proceeds[seller];
+    }
+
+    ////////////////////////////////////
+    // Internal and private Functions //
+    ////////////////////////////////////
+
+    function _getNow() internal view virtual returns (uint256) {
+        return block.timestamp;
     }
 }
