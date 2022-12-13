52ae6046ff0a98fe234dbc10090d1d242d8a3fe5
AsthaGarg16 <asthagarg1611@gmail.com>
2022-12-14 02:28:02 +0530

    frontend updated

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import nft from "../assets/nft.png";
+
+function Home() {
+  return (
+    <div>
+      <img src={require("../assets/nft.jpg")} alt="nft" />
+    </div>
+  );
+}
+
+export default Home;
