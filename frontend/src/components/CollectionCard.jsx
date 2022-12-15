1f990d9e105b58579fdab49366615fb1bd13654c
AsthaGarg16 <asthagarg1611@gmail.com>
2022-12-15 23:37:46 +0530

    minor css

--- frontend/src/components/CollectionCard.jsx
+++ frontend/src/components/CollectionCard.jsx
@@ -7,17 +7,17 @@ function CollectionCard() {
         <img
           src={require("../assets/nft.jpg")}
           alt="nft"
-          className="object-cover rounded-t-lg w-96 h-48 max-w-96 max-h-48 hover:scale-125 ease-in duration-300"
+          className="object-cover rounded-t-lg w-96 h-48 max-w-96 max-h-48 hover:scale-150 ease-in duration-300"
         />
       </div>
 
       <div className="flex items-center p-3 space-x-5">
         <img
-          class="w-16 h-16 object-cover rounded-full shadow-lg border-4 border-white dark:border-gray-400"
+          class="w-16 h-16 object-cover rounded-xl shadow-lg border-4 border-white dark:border-gray-400"
           src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt="Bonnie image"
         />
-        <div class="text-left">
+        <div class="text-left items-center">
           <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
             Collection name
           </h5>
--- frontend/src/components/DetailCard.jsx
+++ frontend/src/components/DetailCard.jsx
@@ -17,42 +17,50 @@ function DetailCard() {
         </h5>
 
         <div className="flex items-center mt-2.5 mb-2.5"></div>
-        <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            $599
-          </span>
-          <button
-            type="button"
-            className="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg p-2 text-center inline-flex items-center dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus:ring-indigo-400"
-          >
-            <svg
-              aria-hidden="true"
-              className="w-8 h-8"
-              fill="currentColor"
-              viewBox="-2 -2 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-            </svg>
+        <div className="grid grid-cols-2">
+          <div>
+            <span className="text-3xl grid justify-items-start font-bold text-gray-900 dark:text-white">
+              $599
+            </span>
+          </div>
 
-            <span className="sr-only">Add to cart</span>
-          </button>
-          <button
-            type="button"
-            className="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg p-2 text-center inline-flex items-center dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus:ring-indigo-400"
-          >
-            <svg
-              aria-hidden="true"
-              className="w-8 h-8"
-              fill="currentColor"
-              viewBox="-2 -2 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+          <div className="grid grid-cols-2 justify-items-end">
+            <button
+              type="button"
+              className="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg p-2 text-center inline-flex items-center dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus:ring-indigo-400"
             >
-              <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-8 h-8"
+                fill="currentColor"
+                class="bi bi-heart-fill"
+                viewBox="-4 -4 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                />
+              </svg>
 
-            <span className="sr-only">Add to cart</span>
-          </button>
+              <span className="sr-only">Like</span>
+            </button>
+            <button
+              type="button"
+              className="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg p-2 text-center inline-flex items-center dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus:ring-indigo-400"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8"
+                fill="currentColor"
+                viewBox="-2 -2 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+              </svg>
+
+              <span className="sr-only">Add to cart</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
