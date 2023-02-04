b108dfbe2c17d94c22dfa1abed128cd94d9ace97
AsthaGarg16 <asthagarg1611@gmail.com>
2023-02-04 15:39:34 +0800

    cart modal open

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-gray-900">
       {({ open }) => (
         <>
-          <CartModal handleClose={closeCart} show={showCartModal} />
+          <CartModal handleClose={closeCart} show={showCart} />
           <div className="mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
